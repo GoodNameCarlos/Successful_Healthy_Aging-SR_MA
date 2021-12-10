c06df5311ea5235264a64bbfe3d6cb86f087c5ff
Carlos Gonzalez-Carballo <carlos.gz.cb@gmail.com>
2021-12-10 16:25:21 -0600

    Se completaron las funciones, se necesita terminar la funciÃon que las junta

--- Extracción de datos_fun.R
+++ Extracción de datos_fun.R
@@ -93,7 +93,7 @@ tables_extract_dbl <- function(.data, variables, separator, icvar) {
   }
 }
 
-tables_extract_dbl(andrews, X1:X3, separator = "–", icvar = X3)
+andrews <- tables_extract_dbl(andrews, X1:X3, separator = "–", icvar = X3)
           
           # Referencia 
           andrews %>% 
@@ -108,25 +108,39 @@ tables_extract_dbl(andrews, X1:X3, separator = "–", icvar = X3)
             mutate_at(c("OR", "lower", "upper"), as.numeric)
 
 # Se agregan la informaciond el autor, tipo de efecto, y cambio a minuculas
-andrews <- andrews %>% 
-  mutate(author_year = "Britton (2008)",
-         or_rr = "OR",
-         characteristics = str_to_lower(characteristics))
+tables_fin <- function(.data, ) {
   
-# Ejercicio con Arroyo Quiroz ---------------------------------------------
+}
+          # Referencia
+          andrews <- andrews %>% 
+            mutate(author_year = "Britton (2008)",
+                   or_rr = "OR",
+                   characteristics = str_to_lower(characteristics))
+
+# Producto final 
+andrews
+
+
+# El problema con puntuaciones: Arroyo Quiroz ---------------------------------------------
 arroyo_quiroz <- pdf_text(pdf = here("Articulos", "Arroyo-Quiroz (2020).pdf"))
 arroyo_quiroz_HR <- arroyo_quiroz[15]
-tab <- str_split(arroyo_quiroz_HR, "\n")
-tab <- tab[[1]]
-table <- c(tab[17:25], tab[31:37]) 
-table <- table[c(-5, -8)]
-
-
+  tab <- str_split(arroyo_quiroz_HR, "\n")
+  tab <- tab[[1]]
+  table <- c(tab[17:25], tab[31:37]) 
+  table <- table[c(-5, -8)]
+  
 arroyo_quiroz <- pdf_tables(table)
 
-tables_extract_punct(.data = arroyo_quiroz, variables = X1:X3, separator = ",", IC_VAR = X3)
+arroyo_quiroz
 
+## el problema es que aquí se transforma en double 
+tables_extract_dbl(arroyo_quiroz, X1:X3, ",", icvar = X3) 
+
+## Se genera la función para responder a esa problemática 
 tables_extract_punct <- function(.data, variables, separator, IC_VAR) {
+  require(tidyverse) 
+  require(stringr)
+  
   if (is_tibble(.data) == TRUE) {
     .data %>% 
       select({{ variables }}) %>% 
@@ -136,14 +150,125 @@ tables_extract_punct <- function(.data, variables, separator, IC_VAR) {
       select(-tmp_chunks, -{{ IC_VAR }}) %>% 
       rename(characteristics = X1, 
              OR = X2) %>% 
-      mutate(OR = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}"),
-             lower = str_extract(string = lower, pattern = "\\d{1,2}.\\d{1,2}|.\\d{1,2}"),  
-             upper = str_extract(string = upper, pattern = "\\d{1,2}.\\d{1,2}|.\\d{1,2}")) 
+      mutate(OR = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}"), 
+             lower = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}"), 
+             upper = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}")) %>% 
+      mutate_at(c("OR", "lower", "upper"), as.numeric)
+  } else {
+    warning("check data")
+  }
+}
+
+tables_extract_punct(.data = arroyo_quiroz, variables = X1:X3, separator = ",", IC_VAR = X3)
+
+
+# Function "tables_extract" complete --------------------------------------
+# chequeo de la función "str_detect" con tabble y tidyverse
+test <- arroyo_quiroz %>% summarise(check = all(str_detect(string = X3, pattern = "[[:punct:]]")))
+
+test == F
+
+# prueba 
+
+Prueba <- function(.data, icvar, punct) {
+  require(tidyverse)
+  require(stringr)
+  
+  # Chequeo de si contiene signos de puntuación 
+  check <- .data %>% summarise(check = all(str_detect(string = {{ icvar }}, pattern = {{ punct }})))
+  
+  if (check == TRUE) { 
+    print("Contains a punct")
+  } else if (check == FALSE) {
+      print("Does not contain a punct")
+  } else {
+      warning("Does not work")
+    }
+}
+
+Prueba(arroyo_quiroz, icvar = X3, punct = "\\(")
+Prueba(andrews, icvar = X3, punct = "\\(")
+
+# Conjuto de las funciones pasadas en una sola
+
+tables_extract <- function(.data, variables, separator, icvar, punct) {
+  require(tidyverse)
+  require(stringr)
+  
+  # Chequeo de si contiene signos de puntuación 
+  check <- .data %>% summarise(check = all(str_detect(string = {{ icvar }}, pattern = {{ punct }})))
+  
+  # Si contiene un signo de puntuación específico
+  if (check == TRUE & is_tibble(.data) == TRUE) { 
+    
+    .data %>% 
+      select({{ variables }}) %>% 
+      mutate(tmp_chunks = str_split({{ IC_VAR }}, fixed({{ separator }}), n = 2)) %>%   
+      mutate(lower = map_chr(tmp_chunks, 1), 
+             upper = map_chr(tmp_chunks, 2)) %>% 
+      select(-tmp_chunks, -{{ IC_VAR }}) %>% 
+      rename(characteristics = X1, 
+             OR = X2) %>% 
+      mutate(OR = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}"), 
+             lower = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}"), 
+             upper = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}")) %>% 
+      mutate_at(c("OR", "lower", "upper"), as.numeric)
+    
+  } else if (check == FALSE) { # Si no contiene un signo de puntuación específico
+    print("Does not contain a punct")
+  } else {
+    warning("Does not work")
+  }
+} 
+
+
+# Deprecated functions ----------------------------------------------------
+
+tables_extract_punct_Deprecated <- function(.data, variables, separator, IC_VAR, punct1, punct2) {
+  if (is_tibble(.data) == TRUE) {
+    .data %>% 
+      select({{ variables }}) %>% 
+      mutate(tmp_chunks = str_split({{ IC_VAR }}, fixed({{ separator }}), n = 2)) %>%   
+      mutate(lower = map_chr(tmp_chunks, 1), 
+             upper = map_chr(tmp_chunks, 2)) %>% 
+      select(-tmp_chunks, -{{ IC_VAR }}) %>% 
+      rename(characteristics = X1, 
+             OR = X2) %>% 
+      mutate(OR = str_extract(string = OR, pattern = "^\\d{1,2}.\\d{1,2}|.\\d{1,2}"), 
+             lower = str_remove(string = lower, pattern = {{ punct1 }}), 
+             upper = str_remove(string = lower, pattern = {{ punct2 }})) %>% 
+      mutate_at(c("OR", "lower", "upper"), as.numeric)
   } else {
     warning("check data")
   }
 }
 
 
-str_view(string = arroyo_quiroz$X3, pattern = "[[:punct:]]\\d{1,2}.\\d{1,2}|.\\d{1,2}")
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
