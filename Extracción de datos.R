4a4b7a05801d9598c8065c11f3878a8604cdf1b5
Carlos Gonzalez-Carballo <carlos.gz.cb@gmail.com>
2021-12-13 16:22:42 -0600

    Add new working script
    Se agrega nuevo script de trabajo para la extracción de los datos de los artículos, considerando las funciones que se crearon.

---
+++ Extracción de datos.R
@@ -0,0 +1,23 @@
+
+################################################################################
+
+# Título: Extracción de datos para meta análisis
+# Proyecto: Factores asociados con el desarrollo de envejecimiento exitoso y 
+#           saludable: Revisión Sistemática y metaanálisis de estudios 
+#           longitudinales. 
+# Autor: Carlos González-Carballo 
+# email: carlos.gz.cb@gmail.com 
+# Script: Extracción de la información de cada artículo a incluir en el metaanálisis
+# Data: PDF de cara artículo incluido en el metaanálisis. 
+# Copyright: Este Script es producto de Carlos González-Carballo como trabajo de
+#            investigación del Doctorado en ciencias, Epidemiología.
+
+#                     STATUS: Stable
+
+################################################################################
+
+
+
+
+
+
