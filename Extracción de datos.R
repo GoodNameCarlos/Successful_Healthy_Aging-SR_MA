f61a3a71bbfa81a2101ca2c2f449577baf20db3c
Carlos Gonzalez-Carballo <carlos.gz.cb@gmail.com>
2021-12-14 14:15:00 -0600

    Articles added
    Andrews & Arroyo-Quiroz

--- Extracción de datos.R
+++ Extracción de datos.R
@@ -20,29 +20,31 @@
 # 1: Extracción de la información. 
 # 2:   
 
+
 ### ------------------------------------------------------------------- ###
 ###                           Artículos                                 ###
 ### ------------------------------------------------------------------- ###
 
+###---------------------------------------------------------------------###
 # Andrews 2002 ------------------------------------------------------------
 
-andrews <- pdf_text(pdf = here("Articulos", "Andrews (2002).pdf")) 
+andrews <- pdftools::pdf_text(pdf = here("Articulos", "Andrews (2002).pdf")) 
 
 ## Level of Function with Activity, Physical Performance and health --------
-andrews_or_health <- andrews[11]
+andrews_or_phys_health <- andrews[11]
 # 1
-tab <- str_split(andrews_or_health, pattern = "\n")
+tab <- str_split(andrews_or_phys_health, pattern = "\n")
   tab <- tab[[1]]
   tab <- tab[8:23] 
   tab <- tab[c(-2, -7:-8)]
 # 2
-andrews_or_health <- pdf_tables(tab)
+andrews_or_phys_health <- pdf_tables(tab)
 # 3
-andrews_or_health <- tables_extract(andrews_or_health, 
+andrews_or_phys_health <- tables_extract(andrews_or_phys_health, 
                                     variables = X1:X3, orvar = X2, icvar = X3, 
                                     separator = "–")
 # 4
-andrews_or_health <- andrews_or_health %>% 
+andrews_or_phys_health <- andrews_or_phys_health %>% 
   mutate(characteristics = recode(characteristics, 
                            "Domestic chores" = "Domestic chores (Adelaide)",
                            "Household maintenance" = "Household maintenance (Adelaide)",
@@ -50,9 +52,10 @@ andrews_or_health <- andrews_or_health %>%
                            "Social activities"  = "Social activities (Adelaide)", 
                            "Moderate" = "Moderate exercise", 
                            "Vigorous" = "Vigorous exercise"))
-tables_fin()
+# 5
+andrews_or_phys_health <- tables_fin(andrews_or_phys_health, author_year = "Andrews 2002", or_rr = "OR")
 
-write.csv2(andrews_or_health, here("Data", "andrews2002_or_health.csv")) 
+write_excel_csv(andrews_or_phys_health, here("Data", "andrews2002_or_phys_health.csv")) 
 
 ## Level of Function with Activity, Physical Performance and health --------
 andrews_or_psych_cog <- andrews[12]
@@ -67,28 +70,117 @@ andrews_or_psych_cog <- tables_extract(andrews_or_psych_cog,
                                        variables = X1:X3, orvar = X2, icvar = X3,
                                        separator = "–")
 # 4 
+andrews_or_psych_cog <- andrews_or_psych_cog %>%
+  mutate(characteristics = recode(characteristics, 
+                                  "Picture Immed." = "Picture Immed. (Memory)",
+                                  "Picture Delay" = "Picture Delay (Memory)",
+                                  "Symbol" = "Symbol (Cog)",
+                                  "Speed" = "Speed (Cog)",
+                                  "Naming" = "Naming (Cog)",
+                                  "NART errors" = "NART errors",
+                                  "Similarities" = "Similarities (Cog)"))
+# 5
+andrews_or_psych_cog <- tables_fin(andrews_or_psych_cog, author_year = "Andrews (2002)", or_rr = "OR")
+
+write_excel_csv(andrews_or_psych_cog, here("Data", "andrews2002_or_psych_cog.csv"))
+
+## Level of function by medical conditions ---------------------------------
+andrews_or_health <- andrews[8]
+# 1 
+tab <- str_split(andrews_or_health, "\n")
+  tab <- tab[[1]]
+  tab <- tab[39:49]
+# 2
+andrews_or_health <- pdf_tables(tab)
+# 3
+andrews_or_health <- tables_extract(andrews_or_health, 
+                                    variables = X1:X3, orvar = X2, icvar = X3, 
+                                    separator = "–")
+# 4 (No rename)
+# 5 
+andrews_or_health <- tables_fin(andrews_or_health, author_year = "Andrews (2002)", or_rr = "OR")
+
+write_excel_csv(andrews_or_health, here("Data", "andrews2002_or_health.csv"))
 
 
+## Otros datos ------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# otros datos 
 per_h_ee <- (302/(302 + 223 + 253))*100
 per_m_ee <- (201/(201 + 155 + 269))*100
 
 
+###---------------------------------------------------------------------###
+# Arroyo Quiroz (2020) ----------------------------------------------------
+arroyo_quiroz <- pdftools::pdf_text(pdf = here("Articulos", "Arroyo-Quiroz (2020).pdf"))
 
+# HR of losing Healthy Aging Status ---------------------------------------
+arroyo_quiroz_HR <- arroyo_quiroz[15]
+# 1
+tab <- str_split(arroyo_quiroz_HR, "\n")
+  tab <- tab[[1]]
+  tab <- tab[c(17:25, 31:37)]
+  tab <- tab[c(-5, -8)]
+
+# 2
+arroyo_quiroz_HR <- pdf_tables(data = tab)
+# 3 
+arroyo_quiroz_HR <- tables_extract(arroyo_quiroz_HR, 
+                                   variables = X1:X3, orvar = X2, icvar = X3, 
+                                   separator = ",", dbl = TRUE)
+# 4
+arroyo_quiroz_HR <- arroyo_quiroz_HR %>% 
+  mutate(characteristics = recode(characteristics, 
+                                  "Former" = "Former smoker",
+                                  "Current" = "Current smoker"))
+# 5
+arroyo_quiroz_HR <- tables_fin(arroyo_quiroz_HR, author_year = "Arroyo-Quiroz (2020)", or_rr = "HR")
+
+write_csv(arroyo_quiroz_HR, here("Data", "arroyo_quiroz2020_HR.csv"))
+
+
+# OR of HA at age 77 ------------------------------------------------------
+arroyo_quiroz_OR77 <- arroyo_quiroz[16]
+# 1 
+tab <- str_split(arroyo_quiroz_OR77, pattern = "\n")
+  tab <- tab[[1]]
+  tab <- tab[c(16:24, 30:36)]
+  tab <- tab[-7]
+  
+# 2
+arroyo_quiroz_OR77 <- pdf_tables(tab)
+# 3 
+tables_extract(arroyo_quiroz_OR77, variables = X1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+#  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
